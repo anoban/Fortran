898018e783add2a2141cdb18e74a1e42d6d5efce
anoban <2016sb117@univ.jfn.ac.lk>
2024-03-20 20:00:53 +0530

    modified:   subroutines.f90 	modified:   types.f90

--- subroutines.f90
+++ subroutines.f90
@@ -1,1 +1,60 @@
 ! let's play with subroutines.
+
+module subroutines
+    use iso_fortran_env
+    implicit none
+
+contains
+
+subroutine quadruple(input, output)
+    integer(int64), intent(inout) :: input  ! input argument, if this is to be quadrupled inplace, specify quadruple = true
+    integer(int64), optional, intent(out) :: output   ! optional argument to store the result
+
+    ! when only one argument is passed to the subroutine, the input variable will be modified inplace
+    ! only when the subroutine is called with two arguments (the second one must be a modifiable lvalue)
+    ! the result will be written to the output
+
+    if (present(output)) then
+        ! when an optional rgument is passed in,
+        ! if inplace = false, the caller must provide a writable output variable
+        output = input * input * input * input
+    else
+        input = input * input * input * input
+    end if
+
+end subroutine quadruple
+
+end module subroutines
+
+subroutine increment(x)
+    use iso_fortran_env
+    implicit none
+    integer(int64), intent(inout) :: x
+    x = x + 1
+end subroutine increment
+
+program main
+    use iso_fortran_env
+    use subroutines
+    implicit none
+
+    integer(int64) :: result = 0, i = 0
+    external increment  ! implicit interface to the freestanding subroutine increment
+    ! since subroutines do not return anything, we do not need to specify the return type in the implicit interface
+
+    do i = 1, 100, 1
+        ! if i is a even number call the subroutine with the optional arg
+        if (mod(i, 2_int64) == 0) then
+            call quadruple(i, result)
+            print*, "i = ", i, "result = ", result
+        else
+            ! ask for an inplace modification, this will shorten the number of loop iterations, as the loop counter will be
+            ! quadrupled in-place
+            call quadruple(i)
+            print*, "i = ", i, "result = ", result
+        end if
+    end do
+
+    call increment(result)
+
+end program main
--- types.f90
+++ types.f90
@@ -12,6 +12,33 @@ type ram
     character(len=100) :: make
 end type
 
+type pair
+    real(real64) :: first, second
+end type pair
 
+    type(pair), allocatable :: tuples(:)
+    ! without explictly initializing these variables, we'll run into floating point exceptions in line 35 & 36
+    real(real128) :: t_first = 0.00, t_second = 0.00, l_first = 0.00, l_second = 0.00
+    integer(int64) :: i
+    integer(int64), parameter :: nelems = 100000
+
+    allocate(tuples(nelems))
+    do i = 1, nelems
+        call random_number(tuples(i)%first)
+        call random_number(tuples(i)%second)
+    end do
+
+    t_first = sum(tuples%first)
+    t_second = sum(tuples%second)
+
+    do i = 1, nelems
+        l_first = l_first + tuples(i)%first
+        l_second = l_second + tuples(i)%second
+    end do
+
+    deallocate(tuples)
+
+    print*, "array sums :: firsts = ", t_first, " seconds = ", t_second
+    print*, "loop sums :: firsts = ", l_first, " seconds = ", l_second
 
 end program main
